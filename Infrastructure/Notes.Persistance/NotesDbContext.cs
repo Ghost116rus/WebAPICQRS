79139576bbf19d302620beabfb33ce3460f088b4
Ghost116rus <danilkaleev@mai.ru>
2023-03-20 02:22:16 +0300

    Добавьте файлы проекта.

---
+++ Infrastructure/Notes.Persistance/NotesDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Aplication.Interfaces;
+using Notes.Domain;
+using Notes.Persistance.EntityTypeConfiguration;
+
+namespace Notes.Persistance
+{
+    public class NotesDbContext : DbContext, INotesDbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        public NotesDbContext(DbContextOptions<NotesDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new NoteConfiguration());
+            base.OnModelCreating(builder);
+        }
+    }
+}
