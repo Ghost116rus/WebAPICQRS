1b8ce514395f50d9351ec84f770d3c62c284f36d
Ghost116rus <danilkaleev@mai.ru>
2023-03-22 16:05:17 +0300

    Внедрение зависимостей изучил

---
+++ Core/Notes.Aplication/Notes/Queries/GetNoteList/NoteLookupDTO.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Notes.Aplication.Common.Mappings;
+using Notes.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Aplication.Notes.Queries.GetNoteList
+{
+    public class NoteLookupDTO : IMapWith<Note>
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Note, NoteLookupDTO>()
+                .ForMember(noteDTO => noteDTO.Id,
+                opt => opt.MapFrom(note => note.Id))
+                .ForMember(noteDTO => noteDTO.Title,
+                opt => opt.MapFrom(note => note.Title));
+        }
+    }
+}
